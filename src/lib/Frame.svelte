9397378f77111c5e37fc4d60be96145c5573205b
Tom Most <twm@freecog.net>
2024-09-01 00:29:40 -0700

    Very crude visualization

--- src/lib/Frame.svelte
+++ src/lib/Frame.svelte
@@ -1,13 +1,53 @@
 <script>
     import { artX, artY, frameX, frameY, frameWidth, rabbetWidth } from "$lib/stores"
+
+    let frameInnerX = $frameX - 2 * $frameWidth;
+    let frameInnerY = $frameY - 2 * $frameWidth;
 </script>
 
 <svg viewBox={`0 0 ${$frameX} ${$frameY}`}>
+    <!-- the art -->
     <rect
         x={$frameWidth - $rabbetWidth}
         y={$frameWidth - $rabbetWidth}
         width={$artX}
         height={$artY}
         fill="#eee"
+    />
+    <!-- the frame -->
+    <path
+        d={`M 0 0
+        l ${$frameX} 0
+        l 0 ${$frameY}
+        l ${-$frameX} 0
+        Z
+
+        M ${$frameWidth} ${$frameWidth}
+        l ${frameInnerX} 0
+        l 0 ${frameInnerY}
+        l ${-frameInnerX} 0
+        Z
+        `}
+        fill="none"
+        stroke="black"
+        stroke-width="0.01px"
+    />
+    <!-- the miter cuts -->
+    <path
+        d={`M 0 0
+        l ${$frameWidth} ${$frameWidth}
+
+        M ${$frameX} 0
+        l ${-$frameWidth} ${$frameWidth}
+
+        M ${$frameX} ${$frameY}
+        l ${-$frameWidth} ${-$frameWidth}
+
+        M 0 ${$frameY}
+        l ${$frameWidth} ${-$frameWidth}
+        `}
+        fill="none"
+        stroke="red"
+        stroke-width="0.01px"
     />
 </svg>
