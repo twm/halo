b7dd53392d4a233b99f7c208c561da943aa583e0
Tom Most <twm@freecog.net>
2024-09-02 19:40:32 -0700

    Screenshot at device scale?

--- tests/test.ts
+++ tests/test.ts
@@ -5,5 +5,5 @@ test("calculator layout", async ({ page }) => {
     const title = "Picture Frame Cutlist Calculator"
     await expect(page.locator("h1")).toHaveText(title)
     await expect(page).toHaveTitle(title)
-    await expect(page).toHaveScreenshot()
+    await expect(page).toHaveScreenshot({ scale: "device" })
 })
